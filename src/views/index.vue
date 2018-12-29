0859cf01e491f7076a87a9735e999bf96233dccc
ChenYCL <aa34913@126.com>
2018-12-29 10:56:10 +0800

    'init'

---
+++ src/views/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="text-align:center">
+    <h2>Demo</h2>
+
+    <div style="position: relative">
+      <sms :ms="60" @fn="smsSend">
+        <span class="content-sms" slot="content">短信验证码</span>
+      </sms>
+    </div>
+  </div>
+</template>
+<script>
+import sms from "../components/smsCountDown";
+export default {
+  name: "index",
+  components: {
+    sms
+  },
+  methods: {
+    smsSend() {
+      console.log("send sms code");
+    }
+  }
+};
+</script>
+<style scoped lang="scss">
+.content-sms {
+  display: inline-block;
+  position: absolute;
+  /*width: 10%;*/
+  top: 33px;
+  right: 15px;
+  border-left: 1px solid #cccccc;
+  padding-left: 20px;
+  padding-right: 10px;
+  color: green;
+  cursor: pointer;
+}
+.input{
+    padding: 15px;
+
+}
+</style>
